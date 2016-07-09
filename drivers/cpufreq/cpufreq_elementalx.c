b6bc53280af6b0df4f297798b4ed15b8d94cced8
FireLord1 <firelord.xda@gmail.com>
2016-07-09 22:36:14 +0530

    cpufreq: update elementalx

--- drivers/cpufreq/cpufreq_elementalx.c
+++ drivers/cpufreq/cpufreq_elementalx.c
@@ -12,118 +12,145 @@
  */
 
 #include <linux/slab.h>
-#include <linux/fb.h>
-#include <linux/msm_kgsl.h>
 #include "cpufreq_governor.h"
 
 /* elementalx governor macros */
 #define DEF_FREQUENCY_UP_THRESHOLD		(90)
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(20)
-#define DEF_ACTIVE_FLOOR_FREQ			(800000)
-#define DEF_GBOOST_MIN_FREQ			(998400)
-#define DEF_MAX_SCREEN_OFF_FREQ			(1094400)
+#define DEF_ACTIVE_FLOOR_FREQ			(960000)
 #define MIN_SAMPLING_RATE			(10000)
-#define DEF_SAMPLING_DOWN_FACTOR		(8)
+#define DEF_SAMPLING_DOWN_FACTOR		(4)
 #define MAX_SAMPLING_DOWN_FACTOR		(20)
-#define FREQ_NEED_BURST(x)			(x < 600000 ? 1 : 0)
+#define FREQ_NEED_BURST(x)			(x < 800000 ? 1 : 0)
 #define MAX(x,y)				(x > y ? x : y)
 #define MIN(x,y)				(x < y ? x : y)
-#define TABLE_SIZE				5
+#define TABLE_SIZE				12
+#define TABLE_NUM				6
 
 static DEFINE_PER_CPU(struct ex_cpu_dbs_info_s, ex_cpu_dbs_info);
 
 static unsigned int up_threshold_level[2] __read_mostly = {95, 85};
-static struct cpufreq_frequency_table *tbl = NULL;
-static unsigned int *tblmap[TABLE_SIZE] __read_mostly;
-static unsigned int tbl_select[4];
-
+ 
 static struct ex_governor_data {
 	unsigned int active_floor_freq;
-	unsigned int max_screen_off_freq;
 	unsigned int prev_load;
-	unsigned int g_count;
-	bool suspended;
-	struct notifier_block notif;
 } ex_data = {
 	.active_floor_freq = DEF_ACTIVE_FLOOR_FREQ,
-	.max_screen_off_freq = DEF_MAX_SCREEN_OFF_FREQ,
 	.prev_load = 0,
-	.g_count = 0,
-	.suspended = false
 };
 
-static void dbs_init_freq_map_table(void)
-{
-	unsigned int min_diff, top1, top2;
-	int cnt, i, j;
-	struct cpufreq_policy *policy;
+static unsigned int tblmap[TABLE_NUM][TABLE_SIZE] __read_mostly = {
 
-	policy = cpufreq_cpu_get(0);
-	tbl = cpufreq_frequency_get_table(0);
-	min_diff = policy->cpuinfo.max_freq;
+	//table 0
+	{
+		616400,
+		757200,
+		840000,
+		960000,
+		1248000,
+		1344000,
+		1478400,
+		1555200,
+		1632000,
+		1728000,
+		1824000,
+		1958400,
+	},
 
-	for (cnt = 0; (tbl[cnt].frequency != CPUFREQ_TABLE_END); cnt++) {
-		if (cnt > 0)
-			min_diff = MIN(tbl[cnt].frequency - tbl[cnt-1].frequency, min_diff);
+	//table 1
+	{
+		773040,
+		899760,
+		1014960,
+		1072560,
+		1248000,
+		1344000,
+		1478400,
+		1555200,
+		1632000,
+		1728000,
+		1824000,
+		1958400,
+	},
+
+	//table 2
+	{
+		851100,
+		956700,
+		1052700,
+		1100700,
+		1350400,
+		1416000,
+		1550400,
+		1627200,
+		1740800,
+		1824000,
+		1920000,
+		2054400,
+	},
+
+	//table 3
+	{
+		616400,
+		757200,
+		840000,
+		960000,
+		1248000,
+		1344000,
+		1478400,
+		1555200,
+		1555200,
+		1555200,
+		1555200,
+		1555200,
+	},
+
+	//table 4
+	{
+		773040,
+		899760,
+		1014960,
+		1072560,
+		1248000,
+		1344000,
+		1478400,
+		1555200,
+		1555200,
+		1555200,
+		1555200,
+		1555200,
+	},
+
+	//table 5
+	{
+		851100,
+		956700,
+		1052700,
+		1100700,
+		1350400,
+		1416000,
+		1550400,
+		1627200,
+		1627200,
+		1627200,
+		1627200,
+		1627200,
 	}
 
-	top1 = (policy->cpuinfo.max_freq + policy->cpuinfo.min_freq) / 2;
-	top2 = (policy->cpuinfo.max_freq + top1) / 2;
+};
 
-	for (i = 0; i < TABLE_SIZE; i++) {
-		tblmap[i] = kmalloc(sizeof(unsigned int) * cnt, GFP_KERNEL);
-		BUG_ON(!tblmap[i]);
-		for (j = 0; j < cnt; j++)
-			tblmap[i][j] = tbl[j].frequency;
-	}
-
-	for (j = 0; j < cnt; j++) {
-		if (tbl[j].frequency < top1) {
-			tblmap[0][j] += MAX((top1 - tbl[j].frequency)/3, min_diff);
-		}
-
-		if (tbl[j].frequency < top2) {
-			tblmap[1][j] += MAX((top2 - tbl[j].frequency)/3, min_diff);
-			tblmap[2][j] += MAX(((top2 - tbl[j].frequency)*2)/5, min_diff);
-			tblmap[3][j] += MAX((top2 - tbl[j].frequency)/2, min_diff);
-		} else {
-			tblmap[3][j] += MAX((policy->cpuinfo.max_freq - tbl[j].frequency)/3, min_diff);
-		}
-
-		tblmap[4][j] += MAX((policy->cpuinfo.max_freq - tbl[j].frequency)/2, min_diff);
-	}
-
-	tbl_select[0] = 0;
-	tbl_select[1] = 1;
-	tbl_select[2] = 2;
-	tbl_select[3] = 4;
-}
-
-static void dbs_deinit_freq_map_table(void)
-{
-	int i;
-
-	if (!tbl)
-		return;
-
-	tbl = NULL;
-
-	for (i = 0; i < TABLE_SIZE; i++)
-		kfree(tblmap[i]);
-}
-
-static inline int get_cpu_freq_index(unsigned int freq)
+static inline int get_cpu_freq_index(unsigned int freq, struct dbs_data *dbs_data)
 {
 	static int saved_index = 0;
 	int index;
-
-	if (!tbl) {
+	
+	if (!dbs_data->freq_table) {
 		pr_warn("tbl is NULL, use previous value %d\n", saved_index);
 		return saved_index;
 	}
 
-	for (index = 0; (tbl[index].frequency != CPUFREQ_TABLE_END); index++) {
-		if (tbl[index].frequency >= freq) {
+	for (index = 0; (dbs_data->freq_table[index].frequency != CPUFREQ_TABLE_END); index++) {
+		if (dbs_data->freq_table[index].frequency >= freq) {
 			saved_index = index;
 			break;
 		}
@@ -138,10 +165,6 @@ static inline unsigned int ex_freq_increase(struct cpufreq_policy *p, unsigned i
 		return p->max;
 	} 
 	
-	else if (ex_data.suspended) {
-		freq = MIN(freq, ex_data.max_screen_off_freq);
-	}
-
 	return freq;
 }
 
@@ -154,8 +177,6 @@ static void ex_check_cpu(int cpu, unsigned int load)
 	unsigned int max_load_freq = 0, freq_next = 0;
 	unsigned int j, avg_load, cur_freq, max_freq, target_freq = 0;
 
-
-
 	cur_freq = policy->cur;
 	max_freq = policy->max;
 
@@ -165,33 +186,8 @@ static void ex_check_cpu(int cpu, unsigned int load)
 	}
 	avg_load = (ex_data.prev_load + load) >> 1;
 
-	if (ex_tuners->gboost) {
-		if (ex_data.g_count < 500 && graphics_boost < 3)
-			++ex_data.g_count;
-		else if (ex_data.g_count > 1)
-			--ex_data.g_count;
-	}
-
-	//gboost mode
-	if (ex_tuners->gboost && ex_data.g_count > 300) {
-				
-		if (avg_load > 40 + (graphics_boost * 10)) {
-			freq_next = max_freq;
-		} else {
-			freq_next = max_freq * avg_load / 100;
-			freq_next = MAX(freq_next, ex_tuners->gboost_min_freq);
-		}
-
-		target_freq = ex_freq_increase(policy, freq_next);
-
-		__cpufreq_driver_target(policy, target_freq, CPUFREQ_RELATION_H);
-
-		goto finished;
-	} 
-
-	//normal mode
 	if (max_load_freq > up_threshold_level[1] * cur_freq) {
-		int index = get_cpu_freq_index(cur_freq);
+		int index = get_cpu_freq_index(cur_freq, dbs_data);
 
 		if (FREQ_NEED_BURST(cur_freq) &&
 				load > up_threshold_level[0]) {
@@ -199,20 +195,20 @@ static void ex_check_cpu(int cpu, unsigned int load)
 		}
 		
 		else if (avg_load > up_threshold_level[0]) {
-			freq_next = tblmap[tbl_select[3]][index];
+			freq_next = tblmap[2 + ex_tuners->powersave][index];
 		}
 		
 		else if (avg_load <= up_threshold_level[1]) {
-			freq_next = tblmap[tbl_select[1]][index];
+			freq_next = tblmap[0 + ex_tuners->powersave][index];
 		}
 	
 		else {
 			if (load > up_threshold_level[0]) {
-				freq_next = tblmap[tbl_select[3]][index];
+				freq_next = tblmap[2 + ex_tuners->powersave][index];
 			}
 		
 			else {
-				freq_next = tblmap[tbl_select[2]][index];
+				freq_next = tblmap[1 + ex_tuners->powersave][index];
 			}
 		}
 
@@ -244,7 +240,7 @@ static void ex_check_cpu(int cpu, unsigned int load)
 				(ex_tuners->up_threshold -
 				 ex_tuners->down_differential);
 
-		if (dbs_info->down_floor && !ex_data.suspended) {
+		if (dbs_info->down_floor) {
 			freq_next = MAX(freq_next, ex_data.active_floor_freq);
 		} else {
 			freq_next = MAX(freq_next, policy->min);
@@ -281,32 +277,6 @@ static void ex_dbs_timer(struct work_struct *work)
 
 	gov_queue_work(dbs_data, dbs_info->cdbs.cur_policy, delay, modify_all);
 	mutex_unlock(&core_dbs_info->cdbs.timer_mutex);
-}
-
-static int fb_notifier_callback(struct notifier_block *this,
-				unsigned long event, void *data)
-{
-	struct fb_event *evdata = data;
-	int *blank;
-
-	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
-		blank = evdata->data;
-		switch (*blank) {
-			case FB_BLANK_UNBLANK:
-				//display on
-				ex_data.suspended = false;
-				break;
-			case FB_BLANK_POWERDOWN:
-			case FB_BLANK_HSYNC_SUSPEND:
-			case FB_BLANK_VSYNC_SUSPEND:
-			case FB_BLANK_NORMAL:
-				//display off
-				ex_data.suspended = true;
-				break;
-		}
-	}
-
-	return NOTIFY_OK;
 }
 
 /************************** sysfs interface ************************/
@@ -357,39 +327,6 @@ static ssize_t store_down_differential(struct dbs_data *dbs_data,
 	return count;
 }
 
-static ssize_t store_gboost(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
-{
-	struct ex_dbs_tuners *ex_tuners = dbs_data->tuners;
-	unsigned int input;
-	int ret;
-	ret = sscanf(buf, "%u", &input);
-
-	if (ret != 1 || input > 1)
-		return -EINVAL;
-
-	if (input == 0)
-		ex_data.g_count = 0;
-
-	ex_tuners->gboost = input;
-	return count;
-}
-
-static ssize_t store_gboost_min_freq(struct dbs_data *dbs_data,
-		const char *buf, size_t count)
-{
-	struct ex_dbs_tuners *ex_tuners = dbs_data->tuners;
-	unsigned int input;
-	int ret;
-	ret = sscanf(buf, "%u", &input);
-
-	if (ret != 1)
-		return -EINVAL;
-
-	ex_tuners->gboost_min_freq = input;
-	return count;
-}
-
 static ssize_t store_active_floor_freq(struct dbs_data *dbs_data,
 		const char *buf, size_t count)
 {
@@ -403,25 +340,6 @@ static ssize_t store_active_floor_freq(struct dbs_data *dbs_data,
 
 	ex_tuners->active_floor_freq = input;
 	ex_data.active_floor_freq = ex_tuners->active_floor_freq;
-	return count;
-}
-
-static ssize_t store_max_screen_off_freq(struct dbs_data *dbs_data,
-		const char *buf, size_t count)
-{
-	struct ex_dbs_tuners *ex_tuners = dbs_data->tuners;
-	unsigned int input;
-	int ret;
-	ret = sscanf(buf, "%u", &input);
-
-	if (ret != 1)
-		return -EINVAL;
-
-	if (input == 0)
-		input = UINT_MAX;
-
-	ex_tuners->max_screen_off_freq = input;
-	ex_data.max_screen_off_freq = ex_tuners->max_screen_off_freq;
 	return count;
 }
 
@@ -440,24 +358,39 @@ static ssize_t store_sampling_down_factor(struct dbs_data *dbs_data,
 	return count;
 }
 
+static ssize_t store_powersave(struct dbs_data *dbs_data, const char *buf,
+		size_t count)
+{
+	struct ex_dbs_tuners *ex_tuners = dbs_data->tuners;
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > 1)
+		return -EINVAL;
+
+	if (input == 0)
+		ex_tuners->powersave = input;
+	else if (input == 1)
+		ex_tuners->powersave = 3;
+
+	return count;
+}
+
 show_store_one(ex, sampling_rate);
 show_store_one(ex, up_threshold);
 show_store_one(ex, down_differential);
-show_store_one(ex, gboost);
-show_store_one(ex, gboost_min_freq);
 show_store_one(ex, active_floor_freq);
-show_store_one(ex, max_screen_off_freq);
 show_store_one(ex, sampling_down_factor);
+show_store_one(ex, powersave);
 declare_show_sampling_rate_min(ex);
 
 gov_sys_pol_attr_rw(sampling_rate);
 gov_sys_pol_attr_rw(up_threshold);
 gov_sys_pol_attr_rw(down_differential);
-gov_sys_pol_attr_rw(gboost);
-gov_sys_pol_attr_rw(gboost_min_freq);
 gov_sys_pol_attr_rw(active_floor_freq);
-gov_sys_pol_attr_rw(max_screen_off_freq);
 gov_sys_pol_attr_rw(sampling_down_factor);
+gov_sys_pol_attr_rw(powersave);
 gov_sys_pol_attr_ro(sampling_rate_min);
 
 static struct attribute *dbs_attributes_gov_sys[] = {
@@ -465,11 +398,9 @@ static struct attribute *dbs_attributes_gov_sys[] = {
 	&sampling_rate_gov_sys.attr,
 	&up_threshold_gov_sys.attr,
 	&down_differential_gov_sys.attr,
-	&gboost_gov_sys.attr,
-	&gboost_min_freq_gov_sys.attr,
 	&active_floor_freq_gov_sys.attr,
-	&max_screen_off_freq_gov_sys.attr,
 	&sampling_down_factor_gov_sys.attr,
+	&powersave_gov_sys.attr,
 	NULL
 };
 
@@ -483,11 +414,9 @@ static struct attribute *dbs_attributes_gov_pol[] = {
 	&sampling_rate_gov_pol.attr,
 	&up_threshold_gov_pol.attr,
 	&down_differential_gov_pol.attr,
-	&gboost_gov_pol.attr,
-	&gboost_min_freq_gov_pol.attr,
 	&active_floor_freq_gov_pol.attr,
-	&max_screen_off_freq_gov_pol.attr,
 	&sampling_down_factor_gov_pol.attr,
+	&powersave_gov_pol.attr,
 	NULL
 };
 
@@ -498,7 +427,7 @@ static struct attribute_group ex_attr_group_gov_pol = {
 
 /************************** sysfs end ************************/
 
-static int ex_init(struct dbs_data *dbs_data)
+static int ex_init(struct dbs_data *dbs_data, struct cpufreq_policy *policy)
 {
 	struct ex_dbs_tuners *tuners;
 
@@ -511,29 +440,21 @@ static int ex_init(struct dbs_data *dbs_data)
 	tuners->up_threshold = DEF_FREQUENCY_UP_THRESHOLD;
 	tuners->down_differential = DEF_FREQUENCY_DOWN_DIFFERENTIAL;
 	tuners->ignore_nice_load = 0;
-	tuners->gboost = 1;
-	tuners->gboost_min_freq = DEF_GBOOST_MIN_FREQ;
 	tuners->active_floor_freq = DEF_ACTIVE_FLOOR_FREQ;
-	tuners->max_screen_off_freq = DEF_MAX_SCREEN_OFF_FREQ;
 	tuners->sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR;
+	tuners->powersave = 0;
 
 	dbs_data->tuners = tuners;
 	dbs_data->min_sampling_rate = MIN_SAMPLING_RATE;
-
-	dbs_init_freq_map_table();
-
-	ex_data.notif.notifier_call = fb_notifier_callback;
-	if (fb_register_client(&ex_data.notif))
-		pr_err("%s: Failed to register fb_notifier\n", __func__);
+	dbs_data->freq_table = cpufreq_frequency_get_table(policy->cpu);
 
 	mutex_init(&dbs_data->mutex);
+
 	return 0;
 }
 
 static void ex_exit(struct dbs_data *dbs_data)
 {
-	dbs_deinit_freq_map_table();
-	fb_unregister_client(&ex_data.notif);
 	kfree(dbs_data->tuners);
 }
 
@@ -547,7 +468,7 @@ static struct common_dbs_data ex_dbs_cdata = {
 	.get_cpu_dbs_info_s = get_cpu_dbs_info_s,
 	.gov_dbs_timer = ex_dbs_timer,
 	.gov_check_cpu = ex_check_cpu,
-	.init = ex_init,
+	.init_ex = ex_init,
 	.exit = ex_exit,
 };
 
