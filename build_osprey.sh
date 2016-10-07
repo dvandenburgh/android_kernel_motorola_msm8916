99c3527a5be664b2267cdcaeea317debe74033ae
FireLord1 <firelord.xda@gmail.com>
2016-10-07 23:21:27 +0530

    script: update it with more easy building
    -because im lazy.
    
    credit: @The-Demon12
    link: https://github.com/The-Demon12/msm8916/blob/main/build.sh
    
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- build_osprey.sh
+++ build_osprey.sh
@@ -1,8 +1,9 @@
 #
-# Copyright � 2016,  Sultan Qasim Khan <sultanqasim@gmail.com> 	
-# Copyright � 2016,  Zeeshan Hussain <zeeshanhussain12@gmail.com> 	      
-# Copyright � 2016,  Varun Chitre  <varun.chitre15@gmail.com>	
+# Copyright � 2016,  Sultan Qasim Khan <sultanqasim@gmail.com>
+# Copyright � 2016,  Zeeshan Hussain <zeeshanhussain12@gmail.com>
+# Copyright � 2016,  Varun Chitre  <varun.chitre15@gmail.com>
 # Copyright � 2016,  Aman Kumar  <firelord.xda@gmail.com>
+# Copyright � 2016,  Kartik Bhalla <kartikbhalla12@gmail.com> 
 
 # Custom build script
 #
@@ -19,6 +20,9 @@
 #
 
 #!/bin/bash
+KERNEL_DIR=~/android/kernel/motorola/msm8916
+KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage
+DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
 BUILD_START=$(date +"%s")
 blue='\033[0;34m'
 cyan='\033[0;36m'
@@ -30,21 +34,61 @@ export SUBARCH=arm
 export CROSS_COMPILE=~/android/kernel/toolchain/uber-6.0/bin/arm-eabi-
 export KBUILD_BUILD_USER="haha"
 export KBUILD_BUILD_HOST="FireLord"
-echo -e "$cyan***********************************************"
-echo "          Compiling FireKernel kernel          "
-echo -e "***********************************************$nocol"
 rm -f arch/arm/boot/dts/*.dtb
 rm -f arch/arm/boot/dt.img
 rm -f flash_zip/boot.img
-echo -e " Initializing defconfig"
+echo -e "$yellow ~~~~~~~~~~Initializing defconfig~~~~~~~~~~ $nocol"
 make osprey_defconfig
-echo -e " Building kernel"
-make -j12 zImage
-make -j12 dtbs
 
-~/android/kernel/motorola/msm8916/tools/dtbToolCM -2 -o ~/android/kernel/motorola/msm8916/arch/arm/boot/dt.img -s 2048 -p ~/android/kernel/motorola/msm8916/scripts/dtc/ ~/android/kernel/motorola/msm8916/arch/arm/boot/dts/
+compile_kernel ()
+{
+  echo -e "$red     ~~~~~~~~~~Building kernel~~~~~~~~~~      $nocol"
+  make -j12 zImage
+  if ! [ -a $KERN_IMG ];
+  then
+    echo -e "$blue Kernel Compilation failed! Fix the errors! $nocol"
+    exit 1
+  fi
+  echo -e "$red     ~~~~~~~~~~Making DTB~~~~~~~~~~      $nocol"
+  make -j12 dtbs
+  $DTBTOOL -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+  echo -e "$red     ~~~~~~~~~~Building modules~~~~~~~~~~      $nocol"
+  make -j12 modules
+}
 
-make -j12 modules
+fire_kernel ()
+{
+  echo -e "$cyan***********************************************"
+  echo "          Compiling FireKernel kernel          "
+  echo -e "***********************************************$nocol"
+  echo -e " "
+  echo -e " SELECT ONE OF THE FOLLOWING TYPES TO BUILD : "
+  echo -e " 1.DIRTY"
+  echo -e " 2.CLEAN"
+  echo -n " YOUR CHOICE : ? "
+  read ch
+  echo -n " VERSION : ? "
+  read ver
+  echo -n " OLD VERSION : ? "
+  read old
+  echo -n " Which device : ? "
+  read dev
+  echo -n " Which android mm or n : ? "
+  read anv
+}
+
+case $ch in
+  1) echo -e "$cyan     ~~~~~~~~~~Dirty~~~~~~~~~~ $nocol"
+     echo -e "$cyan     ~~~~~~~~~~Building now~~~~~~~~~~ $nocol"
+     compile_kernel ;;
+  2) echo -e "$cyan     ~~~~~~~~~~Clean $nocol"
+     echo -e "$cyan     ~~~~~~~~~~Building now~~~~~~~~~~ $nocol"
+     make clean
+     make mrproper
+     compile_kernel ;;
+  *) device ;;
+esac
+
 echo -e " Converting the output into a flashable zip"
 rm -rf firekernel_install
 mkdir -p firekernel_install
@@ -56,7 +100,10 @@ cp arch/arm/boot/dt.img flash_zip/tools/
 rm -f ~/android/kernel/upload/osprey/fire_kernel.zip
 cd flash_zip
 zip -r ../arch/arm/boot/fire_kernel.zip ./
-mv ~/android/kernel/motorola/msm8916/arch/arm/boot/fire_kernel.zip ~/android/kernel/upload/osprey/fire_kernel.zip
+today=$(date +"-%Y%m%d-%H:%M")
+mv ~/android/kernel/motorola/msm8916/arch/arm/boot/fire_kernel.zip ~/android/kernel/upload/osprey/FireKernel-$anv-$dev-v$ver$today.zip
+
+fire_kernel
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
